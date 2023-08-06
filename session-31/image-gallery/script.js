15416669a5c1aa5980fe035ed310a31826a36c68
XERXES <eng.asghari75@gmail.com>
2023-08-06 08:37:36 -0700

    image gallery

---
+++ session-31/image-gallery/script.js
@@ -0,0 +1,13 @@
+
+
+
+function selectImage(img){
+    let selectedImage = document.querySelector(".selected-image");
+    selectedImage.classList.remove("selected-image");
+
+    let imageSrc = img.src;
+    document.getElementById("big-image").src = imageSrc;
+    img.classList.add("selected-image");
+
+    
+}
