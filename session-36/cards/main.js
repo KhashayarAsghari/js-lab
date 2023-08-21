39e37097270912fa30dce2ce6ce0d4db26aafff3
XERXES <eng.asghari75@gmail.com>
2023-08-21 05:46:10 -0700

    card

---
+++ session-36/cards/main.js
@@ -0,0 +1,38 @@
+let container = document.getElementById("container");
+
+let cardSample = container.innerHTML;
+
+
+const CARDS = [
+    {
+        imageUrl: "./images/0.webp",
+        title: "دوربین رنگی پنگی",
+        price: "2995406000",
+        rate: 3
+    },
+    {
+        imageUrl: "./images/1.webp",
+        title: "دوربین خوب",
+        price: "52400000",
+        rate: 4 
+    },
+    {
+        imageUrl: "./images/2.webp",
+        title: "دوربین بهتر",
+        price: "82400000",
+        rate: 5 
+    },
+    {
+        imageUrl: "./images/3.webp",
+        title: "دوربین کار راه انداز",
+        price: "400000",
+        rate: 2 
+    },
+
+]
+
+
+
+for(let i =0; i< CARDS.length;i++){
+    
+}
---
+++ session-36/clickable-slider/main.js
@@ -0,0 +1,29 @@
+
+// dom nodes
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+
+
+// functions
+function slideRight(){
+    
+}
+
+
+
+
+
+
+
+
+// events
+
+left.addEventListener("click", slideRight);
+
+
+
+
+
+
+
+
